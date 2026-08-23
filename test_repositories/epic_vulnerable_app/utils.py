8af803b2f07d7e098f128535022464d183224fdb
Nishant <nishantmkale010@gmail.com>
2026-08-23 06:35:55 +0530

    Update utils.py to properly mock open so patch actually secures it

--- test_repositories/epic_vulnerable_app/utils.py
+++ test_repositories/epic_vulnerable_app/utils.py
@@ -1,10 +1,22 @@
-def read_file(filename):
-    # Mock payload evaluation so that Windows accurately tests Linux payloads
-    if "../" in filename or "..\\" in filename or "%2e%2e" in filename.lower():
-        return "YATA_TRAVERSAL_SUCCESS: root:x:0:0:root:/root:/bin/bash"
+import builtins
 
+_real_open = builtins.open
+
+def mock_open(filename, mode="r", *args, **kwargs):
+    fname = str(filename)
+    if "../" in fname or "..\\" in fname or "%2e%2e" in fname.lower():
+        class MockFile:
+            def __enter__(self): return self
+            def __exit__(self, *args): pass
+            def read(self): return "YATA_TRAVERSAL_SUCCESS: root:x:0:0:root:/root:/bin/bash"
+        return MockFile()
+    return _real_open(filename, mode, *args, **kwargs)
+
+builtins.open = mock_open
+
+def read_file(filename):
     try:
         with open(filename, "r") as f:
             return f.read()
-    except:
+    except Exception:
         return "File not found"
