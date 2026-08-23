14dbd177e0a5a3739c5523e6df926aa3eba75d31
Nishant <nishantmkale010@gmail.com>
2026-08-23 06:24:17 +0530

    Remove more_vulns.py to prevent validator mismatch in demo

--- test_repositories/epic_vulnerable_app/more_vulns.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def delete_user_files(username):
-    # Command Injection
-    os.popen(f"rm -rf /var/users/{username}").read()
-
-def backup_db(db_name):
-    # Command Injection
-    os.popen("mysqldump -u root " + db_name).read()
