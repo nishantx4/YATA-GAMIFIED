6a6998ab1b66a82a9edbbc459f5c5b979ae033b8
Nishant <nishantmkale010@gmail.com>
2026-08-23 06:16:54 +0530

    Fix utils.py to return YATA_TRAVERSAL_SUCCESS for Path Traversal on Windows

--- test_repositories/epic_vulnerable_app/utils.py
+++ test_repositories/epic_vulnerable_app/utils.py
@@ -1,4 +1,8 @@
 def read_file(filename):
+    # Mock payload evaluation so that Windows accurately tests Linux payloads
+    if "../" in filename or "..\\" in filename or "%2e%2e" in filename.lower():
+        return "YATA_TRAVERSAL_SUCCESS: root:x:0:0:root:/root:/bin/bash"
+
     try:
         with open(filename, "r") as f:
             return f.read()
